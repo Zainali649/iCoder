f2aa51a6f530468651a72245445f6d63731e4621
Zainali649 <Zainali649@gmail.com>
2021-09-24 18:44:58 +0500

    initial commit

--- home/models.py
+++ home/models.py
@@ -1,10 +1,6 @@
 from django.db import models
 
 
-# Create your models here.
-# Database ----> Excel workbook
-# Models In Django ----> Table  --------> Sheet
-
 class Contact(models.Model):
     sno = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
